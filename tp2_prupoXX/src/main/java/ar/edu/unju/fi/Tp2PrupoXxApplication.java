a6d00d60cf84c43934ad76b68893c280a067ed8c
beto_ <beto_@DESKTOP-SE5TN0A>
2023-04-17 19:07:42 -0500

    asdasdasd

---
+++ tp2_prupoXX/src/main/java/ar/edu/unju/fi/Tp2PrupoXxApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp2PrupoXxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp2PrupoXxApplication.class, args);
+	}
+
+}
